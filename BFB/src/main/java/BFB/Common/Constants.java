d9503ae78b941e4951a799fae3be33503002ee48
WEKarnesky99 <wekarnesky@gmail.com>
2019-10-22 20:38:20 -0700

    Nightly Version numbers update.

--- BFB/src/main/java/BFB/Common/Constants.java
+++ BFB/src/main/java/BFB/Common/Constants.java
@@ -30,7 +30,7 @@ package BFB.Common;
 public class Constants {
     public final static String AppName = "Battletech Force Balancer",
                         AppDescription = "Battletech Force Balancer",
-                        Version = "0.7.3.20191019",
+                        Version = "0.7.3.20191022",
                         AppRelease = "Nightly",
                         Author = "George Blouin",
                         EMail = "george.blouin@gmail.com",
