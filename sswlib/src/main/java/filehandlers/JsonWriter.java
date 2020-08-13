34e7e7191ccc08986a99a6a2f6d6696c050b0d87
Algebro <algebro@tuta.io>
2020-08-13 11:27:26 -0400

    refactor to use abstract placeable instead of each equipment type

--- sswlib/src/main/java/filehandlers/JsonWriter.java
+++ sswlib/src/main/java/filehandlers/JsonWriter.java
@@ -10,29 +10,14 @@ import java.nio.file.Path;
 public class JsonWriter {
     private Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
 
-    public void Write(RangedWeapon weapon, Path outDir) throws Exception {
-        String filename = weapon.LookupName().replace("/", "_") + ".json";
+    public void Write(abPlaceable equipment, Path outDir) throws Exception {
+        String filename = equipment.LookupName().replace("/", "_") + ".json";
         FileWriter fw = new FileWriter(outDir.resolve(filename).toString());
-        gson.toJson(weapon, fw);
+        gson.toJson(equipment, fw);
         fw.flush();
         fw.close();
     }
 
-    public void Write(PhysicalWeapon weapon, Path outDir) throws Exception {
-        String filename = weapon.LookupName().replace("/", "_") + ".json";
-        FileWriter fw = new FileWriter(outDir.resolve(filename).toString());
-        gson.toJson(weapon, fw);
-        fw.flush();
-        fw.close();
-    }
-
-    public void Write(Equipment equipment, Path outDir) throws Exception {
-
-    }
-
-    public void Write(Ammunition ammo, Path outDir) throws Exception {
-
-    }
 
     public void Write(Quirk quirk, Path outDir) throws Exception {
 
