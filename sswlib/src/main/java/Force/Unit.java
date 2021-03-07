d272c5f13cf5941f34cb3bf8c0cf7189a3a0a067
George Blouin <gblouin@compassion.com>
2021-03-06 19:12:52 -0700

    Use EncodeFluff for MUL
    This section was missed when the EncodeFluff method was added.  This will now convert " into &quot; along with other conversions necessary for XML.

--- sswlib/src/main/java/Force/Unit.java
+++ sswlib/src/main/java/Force/Unit.java
@@ -137,7 +137,7 @@ public class Unit implements ifSerializable {
             setOmni(true);
             Configuration = m.GetLoadout().GetName();
         }
-        
+
         if ( !m.GetSSWImage().isEmpty()) {
             UnitImage = m.GetSSWImage();
         }
@@ -196,7 +196,7 @@ public class Unit implements ifSerializable {
             setOmni(true);
             Configuration = vee.GetLoadout().GetName();
         }
-        
+
         if ( !vee.GetSSWImage().isEmpty() ) {
             UnitImage = vee.GetSSWImage();
         }
@@ -233,7 +233,7 @@ public class Unit implements ifSerializable {
         this.v = vee;
         Refresh();
     }
-    
+
     public Unit( BattleForceStats stat ) {
         this();
         Name = stat.getName();
@@ -384,7 +384,7 @@ public class Unit implements ifSerializable {
                 break;
         }
     }
-    
+
     public void UpdateByVehicle() {
         LoadUnit();
         if ( v != null ) {
@@ -441,7 +441,7 @@ public class Unit implements ifSerializable {
             StatsCalced = true;
         }
     }
-    
+
     public void UpdateByMech() {
         LoadUnit();
         if ( m != null ) {
@@ -539,7 +539,7 @@ public class Unit implements ifSerializable {
         this.Model.replace("Alt", "");
         this.Model.trim();
 
-        file.write(CommonTools.tab + "<entity chassis=\"" + this.Name + "\" model=\"" + this.Model + "\">");
+        file.write(CommonTools.tab + "<entity chassis=\"" + FileCommon.EncodeFluff(this.Name) + "\" model=\"" + FileCommon.EncodeFluff(this.Model) + "\">");
         file.newLine();
         file.write(CommonTools.tab + CommonTools.tab + "<pilot name=\"" + this.getMechwarrior() + "\" gunnery=\"" + this.getGunnery() + "\" piloting=\"" + this.getPiloting() + "\" />");
         file.newLine();
@@ -688,7 +688,7 @@ public class Unit implements ifSerializable {
             StatsCalced = true;
         }
     }
-    
+
     private void LoadVehicle() throws Exception {
         CVReader reader = new CVReader();
         String baseDirectory = sswPrefs.get("ListPath", "");
@@ -702,7 +702,7 @@ public class Unit implements ifSerializable {
         }
         if ( !v.GetSSWImage().isEmpty() )
             this.UnitImage = v.GetSSWImage();
-        
+
         if ( BFStats.getPointValue() == 0 ) {
             BFStats = new BattleForceStats(v);
         }
@@ -750,7 +750,7 @@ public class Unit implements ifSerializable {
             StatsCalced = true;
         }
     }
-    
+
     public BattleForceStats getBFStats() {
         if ( BFStats != null ) {
             BFStats.setName(this.Name);
@@ -833,7 +833,7 @@ public class Unit implements ifSerializable {
     public final void setOmni(boolean isOmni) {
         this.isOmni = isOmni;
     }
-    
+
     public String getInfo() {
         return Info;
     }
@@ -863,7 +863,7 @@ public class Unit implements ifSerializable {
         }
         return val;
     }
-    
+
     public boolean HasC3() {
         return C3Available;
     }
@@ -880,11 +880,11 @@ public class Unit implements ifSerializable {
     public String getFullName() {
         return (Name + " " + Model + " " + Configuration).replace("  ", " ").trim();
     }
-    
+
     public String getImage() {
         return UnitImage;
     }
-    
+
     public void SetCurLoadout( String loadout) {
         switch(UnitType) {
             case CommonTools.BattleMech:
@@ -894,7 +894,7 @@ public class Unit implements ifSerializable {
                 v.SetCurLoadout(loadout);
         }
     }
-    
+
     public void SetUnitImage( String image ) {
         UnitImage = image;
         switch(UnitType) {
@@ -906,7 +906,7 @@ public class Unit implements ifSerializable {
                 break;
         }
     }
-    
+
     public void SaveUnit() {
         try
         {
