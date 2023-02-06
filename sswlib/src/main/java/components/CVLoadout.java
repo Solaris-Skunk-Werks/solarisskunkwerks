9442fcb5c3b885013ae4eb340c31c402ada44128
George Blouin <GBlouin@us.ci.org>
2023-02-05 21:43:59 -0700

    Clone Booster
    For Omni variants

--- sswlib/src/main/java/components/CVLoadout.java
+++ sswlib/src/main/java/components/CVLoadout.java
@@ -864,6 +864,9 @@ public class CVLoadout implements ifCVLoadout, ifLoadout {
         if( HasSupercharger() ) {
             clone.SetSupercharger( SCharger );
         }
+        if (HasVTOLBooster()) {
+            clone.SetVTOLBooster( VBooster );
+        }
         if( Owner.IsOmni() ) {
             clone.SetBaseLoadout( this );
         }
@@ -1137,6 +1140,15 @@ public class CVLoadout implements ifCVLoadout, ifLoadout {
         }
         Owner.setChanged(true);
     }
+
+    public void SetVTOLBooster(VTOLBooster s) {
+        // this sets the loadout's booster to a different one.
+        // Used for cloning purposes only!
+        VBooster = s;
+        AddMechModifier(VBooster.GetMechModifier());
+        Owner.SetChanged( true );
+    }
+
     public boolean HasVTOLBooster() { return UsingVTOLBooster; }
     public VTOLBooster GetVTOLBooster() {
         return VBooster;
